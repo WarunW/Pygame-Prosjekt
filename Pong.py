32da68071a36c832f12a3057ed8e61ab65e4902e
warunW <wawia002@osloskolen.no>
2026-02-02 10:41:21 +0100

    Ferdig  Spill

--- Pong.py
+++ Pong.py
@@ -10,6 +10,7 @@ class Ball:
         self._y_pos = y_pos
         self._fart_x = fart_x
         self._fart_y = fart_y
+        self._standard = (x_pos, y_pos, fart_x, fart_y)
 
     def tegneBall(self):
         pg.draw.rect(self._vindu, self._farge, (self._x_pos, self._y_pos, 30, 30))
@@ -18,18 +19,36 @@ class Ball:
         self._x_pos += self._fart_x
         self._y_pos += self._fart_y
 
-    def grenserBall(self, spiller, motstander):
+    def grenserBall(self, spiller, motstander, poengSpiller):
         if self._y_pos <= 0 or self._y_pos >= VINDU_HOYDE - 30:
             self._fart_y *= -1
 
-        if self._y_pos + 30 > spiller._y_pos and ball._y_pos < spiller._y_pos + 100 and self._x_pos + 30 > spiller._x_pos:
+        if self._fart_x > 0 and self._y_pos + 30 > spiller._y_pos and self._y_pos < spiller._y_pos + 100 and self._x_pos + 30 > spiller._x_pos:
             self._fart_x *= -1
+            poengSpiller += 1
 
-        if self._x_pos < motstander._x_pos + 8 and ball._y_pos < spiller._y_pos + 100 and self._y_pos + 30 > spiller._y_pos:
+        if self._x_pos < motstander._x_pos + 8 and self._y_pos < motstander._y_pos + 100 and self._y_pos + 30 > motstander._y_pos:
             self._fart_x *= -1
 
         if self._x_pos <= 0 or self._x_pos >= VINDU_BREDDE - 30:
-            print("gameover")
+            self.restart()
+            self.stopp()
+
+        return poengSpiller
+
+    def stopp(self):
+        self._fart_x = 0
+        self._fart_y = 0
+
+    def restart(self):
+        self._x_pos, self._y_pos, self._fart_x, self._fart_y = self._standard
+
+    def sjekkRestart(self):
+        trykkede_taster = pg.key.get_pressed()
+        if trykkede_taster[pg.K_SPACE] and self._fart_x == 0 and self._fart_y == 0:
+            self.restart()
+            return True
+        return False
 
     def hentY(self):
         return self._y_pos
@@ -57,7 +76,7 @@ class Spiller:
             self._retning = "opp"
         elif trykkede_taster[pg.K_DOWN]:
             self._retning = "ned"
-
+        
     def grenserSpiller(self):
         if self._y_pos < 0:
             self._y_pos = 0
@@ -79,6 +98,9 @@ ball = Ball(vindu,(0,0,0),VINDU_BREDDE/2 - 15, VINDU_HOYDE/2 - 15, 0.4, 0.4)
 spiller = Spiller(vindu, (0,0,0), VINDU_BREDDE - 20, VINDU_HOYDE/2 - 70, 0.5)
 motstander = Spiller(vindu, (0,0,0), 10, VINDU_HOYDE/2 - 70, 0.5)
 
+poengSpiller = 0
+font = pg.font.SysFont(None, 50)
+
 pg.draw.aaline(vindu, (0,0,0), (VINDU_BREDDE/2,0), (VINDU_BREDDE/2, VINDU_HOYDE))
 
 fortsett = True
@@ -88,9 +110,14 @@ while fortsett:
 
     pg.draw.aaline(vindu, (0,0,0), (VINDU_BREDDE/2,0), (VINDU_BREDDE/2, VINDU_HOYDE))
 
+    tekst2 = font.render(str(poengSpiller), True, (0,0,0))
+    vindu.blit(tekst2, (VINDU_BREDDE//2 + 40, 20))
+
     ball.tegneBall()
+    if ball.sjekkRestart():
+        poengSpiller = 0
     ball.flyttBall()
-    ball.grenserBall(spiller, motstander)
+    poengSpiller = ball.grenserBall(spiller, motstander, poengSpiller)
 
     trykkede_taster = pg.key.get_pressed()
     spiller.tegneSpiller()
